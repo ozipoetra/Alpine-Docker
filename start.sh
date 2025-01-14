0681a00fa3289b9e7316c09d8569ddd465973422
ozip <41739417+ozipoetra@users.noreply.github.com>
2025-01-14 16:47:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,4 @@
 #!/bin/sh -e
-if [ ! -d "/data/.config/" || ! -d "/data/.ssh/" ]; then
-  mkdir -p /data/.config
-  mkdir -p /data/.ssh
-fi
-if [ -d "/root/.config/" ]; then
-  rm -rf /root/.config/
-  rm -rf /root/.ssh/
-  ln -s /data/.config /root/
-  ln -s /data/.ssh /root/
-fi
-if [ -d "/data/.config/gh" ]; then
-  while sleep 30; do sh -c "gh cs ssh --repo code50/41739417"; done
-fi
 echo "root:$ROOT_PASSWORD" | chpasswd
 ssh-keygen -A
 exec /usr/sbin/sshd -D -e "$@"
