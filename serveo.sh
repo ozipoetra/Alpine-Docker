cac2c67e5ac8e4063dad11e4b318a6709d4e36f6
ozip <41739417+ozipoetra@users.noreply.github.com>
2025-01-14 14:36:17 +0700

    Update serveo.sh

--- serveo.sh
+++ serveo.sh
@@ -1,2 +1,3 @@
 #!/bin/sh -e
-ssh -o StrictHostKeyChecking=accept-new -R $SERVEO:22:localhost:22 serveo.net
+# ssh -o StrictHostKeyChecking=accept-new -R $SERVEO:22:localhost:22 serveo.net
+ssh -o StrictHostKeyChecking=accept-new $SERVEO@ssh-j.com -N -R $SERVEO:22:localhost:22
