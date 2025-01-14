2026c0b26d2650140af61c97f15b35fcd6f27b1f
ozip <41739417+ozipoetra@users.noreply.github.com>
2025-01-14 14:47:22 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 if [ -d "/root/.config/" ]; then
   rm -rf /root/.config/
-  mkdir -p /root/.config/
-  ln -s /data/.config/ /root/.config/
+  mkdir -p /root/.config
+  ln -s /data/.config /root/.config
 fi
 echo "root:$ROOT_PASSWORD" | chpasswd
 ssh-keygen -A
